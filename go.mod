4ff33fe206256a1e01831664107efc570c83bb41
maria sivrieva <msivrieva@fastly.com>
2022-05-11 15:13:19 +0100

    kk

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/learnings
 
 go 1.17
+
+require github.com/coopernurse/gorp v1.6.1 // indirect
--- tables.go
+++ tables.go
@@ -1,3 +1,7 @@
+package hello
+
+import "github.com/coopernurse/gorp"
+
 type Person struct {
     Id      int64    
     Created int64
@@ -14,3 +18,15 @@ db, err := sql.Open("mymysql", "tcp:localhost:3306*mydb/myuser/mypassword")
 dbmap := &gorp.DbMap{Db: db, Dialect: gorp.MySQLDialect{"InnoDB", "UTF8"}}
 
 table := dbmap.AddTable(Person{}).SetKeys(true, "Id")
+table := dbmap.AddTableWithName(Person{}, "People").SetKeys(true, "Id")
+
+// Adding people to the table:
+
+person := &Person{
+    Created: time.Now().UnixNow(), 
+    Updated: time.Now().UnixNow(),
+    FName: "Joe",
+    LName: "Smith"
+}
+err := dbmap.Insert(person)
+
